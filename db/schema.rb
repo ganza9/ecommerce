8d72cad02fa971f4e2d98ff701dc0edf876dbbc7
Guest User <Guest@epicodus-118.local>
2017-12-05 09:09:53 -0800

    add accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205165447) do
+ActiveRecord::Schema.define(version: 20171205170932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
