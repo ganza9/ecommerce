457176550617acd7a4f400adf65287a1aa528ab6
Guest User <Guest@epicodus-118.local>
2017-12-05 09:25:12 -0800

    add faker gem and seed db:

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.3.0'
+gem 'faker'
 
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Commerce.product_name,
+                        price: Faker::Commerce.price
+                        )
+end
+
+p "Created #{Product.count} products"
